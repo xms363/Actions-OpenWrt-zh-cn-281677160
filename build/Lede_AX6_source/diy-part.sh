707c80d1e6d099d20a780090145908224c587dd4
xms363 <72204373+xms363@users.noreply.github.com>
2021-10-14 20:34:10 +0800

    Update diy-part.sh

--- build/Lede_AX6_source/diy-part.sh
+++ build/Lede_AX6_source/diy-part.sh
@@ -12,9 +12,9 @@ uci set network.lan.netmask='255.255.255.0'                                 # IP
 #uci set network.lan.gateway='192.168.3.1'                                   # IPv4 网关
 #uci set network.lan.broadcast='192.168.2.255'                               # IPv4 广播
 uci set network.lan.dns='223.5.5.5 114.114.114.114'                         # DNS(多个DNS要用空格分开)
-uci set network.lan.delegate='0'                                            # 去掉LAN口使用内置的 IPv6 管理
+#uci set network.lan.delegate='0'                                            # 去掉LAN口使用内置的 IPv6 管理
 uci commit network                                                          # 不要删除跟注释,除非上面全部删除或注释掉了
-#uci set dhcp.lan.ignore='1'                                                 # 关闭DHCP功能
+uci set dhcp.lan.ignore='1'                                                 # 关闭DHCP功能
 #uci commit dhcp                                                             # 跟‘关闭DHCP功能’联动,同时启用或者删除跟注释
 #uci set system.@system[0].hostname='OpenWrt-123'                            # 修改主机名称为OpenWrt-123
 EOF
