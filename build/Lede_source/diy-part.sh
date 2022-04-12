ba5b6e839877c1e80e6d844045948ef67b9f391d
xms363 <72204373+xms363@users.noreply.github.com>
2022-04-12 20:43:19 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -63,6 +63,9 @@ sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ_PATH
 # K3专用，编译K3的时候只会出K3固件（其他机型也适宜,把phicomm_k3替换一下，名字要绝对正确才行）
 #sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm_k3|TARGET_DEVICES += phicomm_k3|' target/linux/bcm53xx/image/Makefile
 
+#拉取Lienol仓库的luci-app-ipsec-server
+svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-ipsec-server package/luci-app-ipsec-server
+
 
 # 在线更新时，删除不想保留固件的某个文件，在EOF跟EOF之间加入删除代码，记住这里对应的是固件的文件路径，比如： rm -rf /etc/config/luci
 cat >$DELETE <<-EOF
