d53137df0940be47f2349d20e6ebe75e53307009
xms363 <72204373+xms363@users.noreply.github.com>
2022-04-13 11:28:52 +0800

    Update diy-part.sh

--- build/Mortal_source/diy-part.sh
+++ build/Mortal_source/diy-part.sh
@@ -74,7 +74,7 @@ sed -i 's/"管理权"/"改密码"/g' `grep "管理权" -rl ./`
 
 # 整理固件包时候,删除您不想要的固件或者文件,让它不需要上传到Actions空间(根据编译机型变化,自行调整删除名称)
 cat >${GITHUB_WORKSPACE}/Clear <<-EOF
-#rm -rf packages
+rm -rf packages
 #rm -rf config.buildinfo
 #rm -rf feeds.buildinfo
 rm -rf openwrt-x86-64-generic-kernel.bin
